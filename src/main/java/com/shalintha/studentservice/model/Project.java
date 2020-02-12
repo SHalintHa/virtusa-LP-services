60bb2fea017a5c5c7fffb2d030d9d0bc2781b32e
SHalintHa <subhashana011@gmail.com>
2020-02-12 05:59:41 +0530

    Modified the controller

--- src/main/java/com/shalintha/studentservice/model/Project.java
+++ src/main/java/com/shalintha/studentservice/model/Project.java
@@ -1,5 +1,7 @@
 package com.shalintha.studentservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -13,6 +15,7 @@ public class Project {
 
 
     @ManyToMany(mappedBy = "projects")
+            @JsonIgnore
     List<Student> students;
 
 
